455fece70909fa22c1159db4e37e6f41a0e09c33
Mario Rene Torres <volaras23@gmail.com>
2015-05-04 10:17:52 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
 package com.craftilandia.blockguard;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
+@Override
+public void onEnable() {
+getServer().getPluginManager().registerEvents(this, this);
+}
+
+@EventHandler
+public void some(BlockBreakEvent e){
+	
+}
+@EventHandler
+public void some(BlockPlaceEvent e){
+	
+}
 
 }
